cb4bbd7915be27cfc0b9277d70cdab43f078de81
Mridang Agarwalla <mridang.agarwalla@gmail.com>
2025-04-14 11:46:58 +0300

    Added tests to ensure that the JWT token based authentication is working as expected

--- src/main/java/com/zitadel/auth/OAuthAuthenticator.java
+++ src/main/java/com/zitadel/auth/OAuthAuthenticator.java
@@ -20,127 +20,127 @@ import java.util.Set;
  */
 public abstract class OAuthAuthenticator extends Authenticator {
 
-  /**
-   * The scope for the token request.
-   */
-  protected final Scope scope;
-  private final OpenId openId;
-  /**
-   * The OAuth token.
-   */
-  @Nullable
-  protected Token token;
-
-  /**
-   * Constructs an OAuthAuthenticator.
-   *
-   * @param openId The URL of the OAuth2 token endpoint.
-   * @param scope  The scope for the token request.
-   */
-  public OAuthAuthenticator(OpenId openId, Scope scope) {
-    super(openId.getHostEndpoint());
-    this.scope = scope;
-    this.token = null;
-    this.openId = openId;
-  }
-
-  public String getAuthToken() {
-    if (token == null || token.isExpired()) {
-      refreshToken();
-    }
-
-    return token.accessToken;
-  }
-
-  /**
-   * Retrieves the authentication headers.
-   * <p>
-   * If no token is available or the current token is expired, refreshes the token.
-   *
-   * @return A map containing the 'Authorization' header.
-   */
-  @Override
-  public Map<String, String> getAuthHeaders() {
-    return Collections.singletonMap("Authorization", "Bearer " + getAuthToken());
-  }
-
-  /**
-   * Refreshes the access token.
-   * <p>
-   * Subclasses must implement this method using their specific OAuth flow.
-   */
-  public abstract Token refreshToken();
-
-  protected abstract AuthorizationGrant getGrant();
-
-  protected Token getToken(ClientAuthentication authentication) {
-    try {
-      URI tokenEndpoint = openId.getTokenEndpoint().toURI();
-      TokenRequest request = new TokenRequest(tokenEndpoint, authentication, this.getGrant(), this.scope);
-      HTTPRequest httpRequest = request.toHTTPRequest();
-      TokenResponse tokenResponse = TokenResponse.parse(httpRequest.send());
-
-      if (!tokenResponse.indicatesSuccess()) {
-        TokenErrorResponse errorResponse = tokenResponse.toErrorResponse();
-        throw new RuntimeException("Token request failed: " + errorResponse.getErrorObject().toString());
-      } else {
-        AccessTokenResponse successResponse = (AccessTokenResponse) tokenResponse;
-        BearerAccessToken accessToken = (BearerAccessToken) successResponse.getTokens().getAccessToken();
-        return new Token(accessToken.getValue(), Instant.now().plusSeconds(accessToken.getLifetime()));
-      }
-    } catch (Exception e) {
-      throw new RuntimeException("Failed to refresh token: " + e.getMessage(), e);
-    }
-  }
-
-  /**
-   * A simple POJO representing an OAuth token.
-   */
-  public static class Token {
-
-    protected final String accessToken;
-    protected final Instant expiresAt;
+    /**
+     * The scope for the token request.
+     */
+    protected final Scope scope;
+    private final OpenId openId;
+    /**
+     * The OAuth token.
+     */
+    @Nullable
+    protected Token token;
 
     /**
-     * Constructs a Token.
+     * Constructs an OAuthAuthenticator.
      *
-     * @param accessToken The access token string.
-     * @param expiresAt   The expiration time as an epoch second.
+     * @param openId The URL of the OAuth2 token endpoint.
+     * @param scope  The scope for the token request.
      */
-    private Token(String accessToken, Instant expiresAt) {
-      this.accessToken = accessToken;
-      this.expiresAt = expiresAt;
+    public OAuthAuthenticator(OpenId openId, Scope scope) {
+        super(openId.getHostEndpoint());
+        this.scope = scope;
+        this.token = null;
+        this.openId = openId;
+    }
+
+    public String getAuthToken() {
+        if (token == null || token.isExpired()) {
+            refreshToken();
+        }
+
+        return token.accessToken;
     }
 
     /**
-     * Checks if the token is expired.
+     * Retrieves the authentication headers.
+     * <p>
+     * If no token is available or the current token is expired, refreshes the token.
      *
-     * @return true if expired; false otherwise.
+     * @return A map containing the 'Authorization' header.
      */
-    private boolean isExpired() {
-      return Instant.now().isAfter(expiresAt);
-    }
-  }
-
-  protected static abstract class OAuthAuthenticatorBuilder<T extends OAuthAuthenticatorBuilder<?>> {
-
-    protected final OpenId openId;
-    protected Scope authScopes = Scope.parse("openid urn:zitadel:iam:org:project:id:zitadel:aud");
-
-    protected OAuthAuthenticatorBuilder(String host) {
-      this.openId = new OpenId(host);
+    @Override
+    public Map<String, String> getAuthHeaders() {
+        return Collections.singletonMap("Authorization", "Bearer " + getAuthToken());
     }
 
     /**
-     * Overrides the default scopes.
-     *
-     * @param authScopes A set of scopes for the token request.
-     * @return The builder instance.
+     * Refreshes the access token.
+     * <p>
+     * Subclasses must implement this method using their specific OAuth flow.
      */
-    @SuppressWarnings("unchecked")
-    public final T scopes(Set<String> authScopes) {
-      this.authScopes = Scope.parse(authScopes);
-      return (T) this;
+    public abstract Token refreshToken();
+
+    protected abstract AuthorizationGrant getGrant();
+
+    protected Token getToken(ClientAuthentication authentication) {
+        try {
+            URI tokenEndpoint = openId.getTokenEndpoint().toURI();
+            TokenRequest request = new TokenRequest(tokenEndpoint, authentication, this.getGrant(), this.scope);
+            HTTPRequest httpRequest = request.toHTTPRequest();
+            TokenResponse tokenResponse = TokenResponse.parse(httpRequest.send());
+
+            if (!tokenResponse.indicatesSuccess()) {
+                TokenErrorResponse errorResponse = tokenResponse.toErrorResponse();
+                throw new RuntimeException("Token request failed: " + errorResponse.getErrorObject().toString());
+            } else {
+                AccessTokenResponse successResponse = (AccessTokenResponse) tokenResponse;
+                BearerAccessToken accessToken = (BearerAccessToken) successResponse.getTokens().getAccessToken();
+                return new Token(accessToken.getValue(), Instant.now().plusSeconds(accessToken.getLifetime()));
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to refresh token: " + e.getMessage(), e);
+        }
     }
-  }
+
+    /**
+     * A simple POJO representing an OAuth token.
+     */
+    public static class Token {
+
+        protected final String accessToken;
+        protected final Instant expiresAt;
+
+        /**
+         * Constructs a Token.
+         *
+         * @param accessToken The access token string.
+         * @param expiresAt   The expiration time as an epoch second.
+         */
+        private Token(String accessToken, Instant expiresAt) {
+            this.accessToken = accessToken;
+            this.expiresAt = expiresAt;
+        }
+
+        /**
+         * Checks if the token is expired.
+         *
+         * @return true if expired; false otherwise.
+         */
+        private boolean isExpired() {
+            return Instant.now().isAfter(expiresAt);
+        }
+    }
+
+    protected static abstract class OAuthAuthenticatorBuilder<T extends OAuthAuthenticatorBuilder<?>> {
+
+        protected final OpenId openId;
+        protected Scope authScopes = Scope.parse("openid urn:zitadel:iam:org:project:id:zitadel:aud");
+
+        protected OAuthAuthenticatorBuilder(String host) {
+            this.openId = new OpenId(host);
+        }
+
+        /**
+         * Overrides the default scopes.
+         *
+         * @param authScopes A set of scopes for the token request.
+         * @return The builder instance.
+         */
+        @SuppressWarnings("unchecked")
+        public final T scopes(Set<String> authScopes) {
+            this.authScopes = Scope.parse(authScopes);
+            return (T) this;
+        }
+    }
 }
