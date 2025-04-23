76ed6dfbbbd1c5190bba9683a93e77f5aac594cc
Mridang Agarwalla <mridang.agarwalla@gmail.com>
2025-04-23 13:07:18 +0300

    Improved the spec tests by adding some tests for the session service

--- src/spec/java/com/zitadel/BaseTest.java
+++ src/spec/java/com/zitadel/BaseTest.java
@@ -7,33 +7,34 @@ import org.junit.jupiter.api.BeforeAll;
 import java.util.Set;
 
 /**
-* BaseTest class provides a setup for loading environment variables from a
-* .env file before any tests are executed. This ensures that the environment
-* properties are available as system properties for all tests in the suite.
-*/
+ * BaseTest class provides a setup for loading environment variables from a
+ * .env file before any tests are executed. This ensures that the environment
+ * properties are available as system properties for all tests in the suite.
+ */
 public class BaseTest {
 
-/**
-* Loads environment variables from a .env file and sets them as system properties
-* before any tests are run. This method is executed once before all tests in the
-* test class. It uses the Dotenv library to read the .env file from the project's
-* root directory and sets each entry as a system property.
-*
-* @throws IllegalArgumentException if there are issues with loading environment variables
-*/
-@BeforeAll
-public static void loadEnvVars() {
-	// Configures Dotenv to load the .env file from the current user's directory.
-	Dotenv dotenv = Dotenv.configure()
-	.directory(System.getProperty("user.dir"))
-	.load();
+    /**
+     * Loads environment variables from a .env file and sets them as system properties
+     * before any tests are run. This method is executed once before all tests in the
+     * test class. It uses the Dotenv library to read the .env file from the project's
+     * root directory and sets each entry as a system property.
+     *
+     * @throws IllegalArgumentException if there are issues with loading environment variables
+     */
+    @BeforeAll
+    public static void loadEnvVars() {
+        // Configures Dotenv to load the .env file from the current user's directory.
+        Dotenv dotenv = Dotenv.configure()
+            .directory(System.getProperty("user.dir"))
+            .load();
 
-	// Retrieves all entries from the .env file.
-	Set<DotenvEntry> envVars = dotenv.entries();
+        // Retrieves all entries from the .env file.
+        Set<DotenvEntry> envVars = dotenv.entries();
+        System.out.println(envVars);
 
-	// Iterates over each entry and sets the key-value pair as a system property.
-	for (DotenvEntry entry : envVars) {
-	System.setProperty(entry.getKey(), entry.getValue());
-	}
-}
+        // Iterates over each entry and sets the key-value pair as a system property.
+        for (DotenvEntry entry : envVars) {
+            System.setProperty(entry.getKey(), entry.getValue());
+        }
+    }
 }
--- src/spec/java/com/zitadel/SDKTestUsingWebTokenAssertionAuthenticationSpec.java
+++ src/spec/java/com/zitadel/SDKTestUsingWebTokenAssertionAuthenticationSpec.java
@@ -13,70 +13,70 @@ import java.util.UUID;
 import static org.junit.jupiter.api.Assertions.fail;
 
 /**
-* This spec tests the SDK behavior using Personal Access Token (PAT) authentication.
-* It includes test cases for valid and invalid token scenarios related to user deactivation and reactivation.
-*/
+ * This spec tests the SDK behavior using Personal Access Token (PAT) authentication.
+ * It includes test cases for valid and invalid token scenarios related to user deactivation and reactivation.
+ */
 @SuppressWarnings("NewClassNamingConvention")
 class SDKTestUsingWebTokenAssertionAuthenticationSpec extends BaseTest {
 
-private final String baseUrl = System.getProperty("BASE_URL");
-private String keyFile;
-private String userId;
+    private final String baseUrl = System.getProperty("BASE_URL");
+    private String keyFile;
+    private String userId;
 
-/**
-* Sets up the test environment before each test by creating a user.
-*/
-@BeforeEach
-void setUp() throws IOException {
-	String k = System.getProperty("JWT_KEY");
-	if (k == null) System.exit(1);
-	File f = File.createTempFile("jwt_", null);
-	Files.write(f.toPath(), k.getBytes(StandardCharsets.UTF_8));
-	keyFile = f.getAbsolutePath();
-	userId = createUser();
-}
+    /**
+     * Sets up the test environment before each test by creating a user.
+     */
+    @BeforeEach
+    void setUp() throws IOException {
+        String k = System.getProperty("JWT_KEY");
+        if (k == null) System.exit(1);
+        File f = File.createTempFile("jwt_", null);
+        Files.write(f.toPath(), k.getBytes(StandardCharsets.UTF_8));
+        keyFile = f.getAbsolutePath();
+        userId = createUser();
+    }
 
-/**
-* Creates a new user in Zitadel using a valid Personal Access Token.
-*
-* @return the user ID of the newly created user
-*/
-private String createUser() {
-  Zitadel zitadel = Zitadel.withPrivateKey(baseUrl, keyFile);
+    /**
+     * Creates a new user in Zitadel using a valid Personal Access Token.
+     *
+     * @return the user ID of the newly created user
+     */
+    private String createUser() {
+        Zitadel zitadel = Zitadel.withPrivateKey(baseUrl, keyFile);
 
-	try {
-	UserServiceAddHumanUserResponse response = zitadel.users.userServiceAddHumanUser(new UserServiceAddHumanUserRequest()
-		.username(UUID.randomUUID().toString())
-		.profile(new UserServiceSetHumanProfile()
-		.givenName("John")
-		.familyName("Doe"))
-		.email(new UserServiceSetHumanEmail()
-		.email("johndoe" + UUID.randomUUID() + "@caos.ag")));
-	System.out.println("User created: " + response);
-	return response.getUserId();
-	} catch (ApiException e) {
-	throw new RuntimeException("Exception while creating user: " + e.getMessage());
-	}
-}
+        try {
+            UserServiceAddHumanUserResponse response = zitadel.users.userServiceAddHumanUser(new UserServiceAddHumanUserRequest()
+                .username(UUID.randomUUID().toString())
+                .profile(new UserServiceSetHumanProfile()
+                    .givenName("John")
+                    .familyName("Doe"))
+                .email(new UserServiceSetHumanEmail()
+                    .email("johndoe" + UUID.randomUUID() + "@caos.ag")));
+            System.out.println("User created: " + response);
+            return response.getUserId();
+        } catch (ApiException e) {
+            throw new RuntimeException("Exception while creating user: " + e.getMessage());
+        }
+    }
 
-/**
-* Tests deactivating and reactivating a user with a valid Personal Access Token.
-* <p>
-* It expects the user to be deactivated and then reactivated successfully.
-*/
-@Test
-void shouldDeactivateAndReactivateUserWithValidToken() {
-	Zitadel zitadel = Zitadel.withPrivateKey(baseUrl, keyFile);
+    /**
+     * Tests deactivating and reactivating a user with a valid Personal Access Token.
+     * <p>
+     * It expects the user to be deactivated and then reactivated successfully.
+     */
+    @Test
+    void shouldDeactivateAndReactivateUserWithValidToken() {
+        Zitadel zitadel = Zitadel.withPrivateKey(baseUrl, keyFile);
 
-	try {
-	UserServiceDeactivateUserResponse deactivateResponse = zitadel.users.userServiceDeactivateUser(userId);
-	System.out.println("User deactivated: " + deactivateResponse);
+        try {
+            UserServiceDeactivateUserResponse deactivateResponse = zitadel.users.userServiceDeactivateUser(userId);
+            System.out.println("User deactivated: " + deactivateResponse);
 
-	UserServiceReactivateUserResponse reactivateResponse = zitadel.users.userServiceReactivateUser(userId);
-	System.out.println("User reactivated: " + reactivateResponse);
-	// assertEquals("success", reactivateResponse.getStatus());
-	} catch (ApiException e) {
-	fail("Exception when calling deactivateUser or reactivateUser with valid token: " + e.getMessage());
-	}
-}
+            UserServiceReactivateUserResponse reactivateResponse = zitadel.users.userServiceReactivateUser(userId);
+            System.out.println("User reactivated: " + reactivateResponse);
+            // assertEquals("success", reactivateResponse.getStatus());
+        } catch (ApiException e) {
+            fail("Exception when calling deactivateUser or reactivateUser with valid token: " + e.getMessage());
+        }
+    }
 }
